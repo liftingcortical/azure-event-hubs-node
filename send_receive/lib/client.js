00ed7d25c6a030c019675ed132a76d321b5ff9fe
Bogdan Duda <bogdan.duda@quantumdatascience.ro>
2017-01-11 17:28:13 +0200

    fix receiver flow policy merging when filter is null

--- send_receive/lib/client.js
+++ send_receive/lib/client.js
@@ -268,7 +268,7 @@ EventHubClient.prototype.createReceiver = function createReceiver(consumerGroup,
                 flowPolicy.creditQuantum = options.flowControlPolicy.creditQuantum;
             }
 
-            filter = amqp10.Policy.merge(flowPolicy, filter);
+            filter = amqp10.Policy.merge(flowPolicy, filter || {});
         }
       }
 
